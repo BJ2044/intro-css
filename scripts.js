bf7d01052bb774c747ce461709148559ab279a06
Brad Jones <BJ2044@gmail.com>
2022-10-24 15:49:07 -0400

    Update

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const family = [
+    {
+        dad: true,
+        name:'Joe',
+        job: 'mechanic'
+    },
+    {
+        kid1: true,
+        name: 'Mia',
+        job: "school"
+    },
+]
+console.log('Hey gang here is my fam', family)
+
+document.getElementById('contact').style.color = 'purple'
+
+const handleColorChange = () => {
+    console.log('changing color')
+}
+
+const handleAlert = () => {
+    alert('Alerting the class')
+}
